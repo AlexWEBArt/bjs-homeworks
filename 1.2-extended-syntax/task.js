2871b4098d96546f5312c1cc5c68421598f849c2
AlexWEBArt <elusiv@mail.ru>
2020-08-24 23:47:28 +0500

    1, 2 tasks finish

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,52 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
-}
+"use strict"
+function getResult(a,b,c) {
+    let coefa = a, coefb = b, coefc = c, x = [];
+    let discriminant = Math.pow(coefb, 2) - 4 * coefa * coefc;
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+    if (discriminant > 0) {
+        x[0] = (-coefb + Math.sqrt(discriminant, 2)) / 2 * coefa;
+        x[1] = (-coefb - Math.sqrt(discriminant, 2)) / 2 * coefa;
+    } else if (discriminant == 0) {
+        x[0] = -coefb / 2 * coefa;
+    } else {
+        x = [];
+    };
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    return x;
+};
+
+
+function getAverageMark(marks) {
+
+    let array = marks;
+    let total = array.slice(0, 5).length;
+    let sum = 0;
+    let averageMark;
+
+    for (let i=0; i < array.slice(0, 5).length; i++) { 
+        sum += array[i];
+    };
+    
+    if (total == 0) {
+        averageMark = 0;
+    } else {
+        averageMark = sum / total;
+    };
+
+    return averageMark;
+};
+
+function askDrink(name,dateOfBirthday) {
+    
+
+    //getFullYear(dateOfBirthday) {
+    //    let age = new Date() - dateOfBirthday;
+    //    return age;
+    //}
+
+    //if getFullYear() > 18 {
+
+    //}
+
+    //return result;
 }
