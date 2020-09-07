307a80ae8aa6c949b4c61c0f08d7f4a4c99a7387
AlexWEBArt <elusiv@mail.ru>
2020-09-08 00:13:27 +0500

    betta

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,43 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"
+
+String.prototype.isPalindrome;
+
+new String("А роза упала на лапу Азора")
+
+function isPalindrome(string) {
+    this.str = string;
+    let strReverse = str.toLowerCase().split('').reverse().join('').split(' ').join('');
+    
+    if (str.toLowerCase().split(' ').join('') === strReverse) {
+        return true;
+    }   else {
+        return false;
+    };
+};
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let averageMark = 0, average = 0, sum = 0, total = marks.length;
+
+    for (let index = 0; index < total; index++) {
+        sum += marks[index];
+        average = sum / total;
+    };
+
+    return averageMark = Math.round(average);
+};
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
+    let date = new Date(), yearMs = 31536000000, verdict;
+    
+    let age = (+date - +birthday) / yearMs;
+
+    
+    if (age >= 18) {
+        verdict = true;
+    }   else {
+        verdict = false;
+    };
+
+    return verdict;
+};
